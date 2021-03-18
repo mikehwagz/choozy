eaed4048209b38d22c5b0588546c20b7b0eefca4
mikehwagz <mike@selfaware.studio>
2021-03-18 11:43:49 -0400

    split on all whitespace characters and not just spaces

--- src/choozy.js
+++ src/choozy.js
@@ -1,8 +1,9 @@
-export default function(container = document.body, prefix = 'js-') {
+export default function (container = document.body, prefix = 'js-') {
   let els = [].slice.call(container.querySelectorAll(`*[class*="${prefix}"]`))
   return els.reduce((acc, el) => {
-    let cx = typeof el.className === 'string' ? el.className : el.className.baseVal
-    let key = cx.split(prefix)[1].split(' ')[0]
+    let cx =
+      typeof el.className === 'string' ? el.className : el.className.baseVal
+    let key = cx.split(prefix)[1].split('\\s+')[0]
     acc[key] = acc[key]
       ? acc[key].constructor === Array
         ? acc[key].concat(el)
