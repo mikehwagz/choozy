92e92b36e67ce962b3a4ed60dcdcebd53c3a896f
mikehwagz <mike@selfaware.studio>
2021-03-18 12:00:36 -0400

    trim key

--- src/choozy.js
+++ src/choozy.js
@@ -3,7 +3,7 @@ export default function (container = document.body, prefix = 'js-') {
   return els.reduce((acc, el) => {
     let cx =
       typeof el.className === 'string' ? el.className : el.className.baseVal
-    let key = cx.split(prefix)[1].split('\\s+')[0]
+    let key = cx.split(prefix)[1].split(' ')[0].trim()
     acc[key] = acc[key]
       ? acc[key].constructor === Array
         ? acc[key].concat(el)
